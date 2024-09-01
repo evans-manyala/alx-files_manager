37157dd842004151da9a16f16350f0ef4aec8e46
Evans Manyala <evans.manyala@gmail.com>
2024-09-02 00:11:21 +0300

    First API

---
+++ controllers/AppController.js
@@ -0,0 +1,16 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+export default class AppController {
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.status(200).json({ redis: true, db: true });
+    }
+  }
+
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    res.status(200).json({ users: usersCount, files: filesCount });
+  }
+}
