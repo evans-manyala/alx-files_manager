34650de9f890059f7c811e3ca0ef5ba9661742f6
Evans Manyala <evans.manyala@gmail.com>
2024-09-02 00:09:27 +0300

    Fixing errors in script

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import routes from './routes/index.js';
+import routes from './routes/index';
 
 const app = express();
 
 app.use('/', routes);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  console.log(`Server is running on port ${port}`);
+});
