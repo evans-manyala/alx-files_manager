c0d0cee14b7d338f0eb769a627b82e64be0434dd
Evans Manyala <evans.manyala@gmail.com>
2024-09-04 23:21:36 +0300

    Authenticate a user

---
+++ controllers/AuthController.js
@@ -0,0 +1,61 @@
+const crypto = require('crypto');
+const { v4: uuidv4 } = require('uuid');
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AuthController {
+  static async getConnect(req, res) {
+    const authHeader = req.headers.authorization || '';
+
+    const base64Credentials = authHeader.split(' ')[1] || '';
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [email, password] = credentials.split(':');
+
+    if (!email || !password) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+    try {
+      const user = await dbClient.db.collection('users').findOne({ email, password: hashedPassword });
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const token = uuidv4();
+      const redisKey = `auth_${token}`;
+      await redisClient.set(redisKey, user._id.toString(), 'EX', 24 * 60 * 60);
+
+      return res.status(200).json({ token });
+    } catch (error) {
+      console.error('Error during authentication:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+      const redisKey = `auth_${token}`;
+      const userId = await redisClient.get(redisKey);
+
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      await redisClient.del(redisKey);
+      return res.status(204).send();
+    } catch (error) {
+      console.error('Error during sign out:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = AuthController;
